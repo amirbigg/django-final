55e7ede8767e8f5cedb4c304275755804ecf7b22
amirbigg <amirbig44@gmail.com>
2020-11-12 22:56:45 +0330

    add ep3

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
 	path('register/', views.UserRegister.as_view(), name='register'),
+	path('login/', views.UserLogin.as_view(), name='login'),
+	path('logout/', views.UserLogout.as_view(), name='logout'),
+	path('dashboard/<str:username>/', views.UserDashboard.as_view(), name='dashboard')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginFrom
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class UserRegister(View):
@@ -21,3 +23,50 @@ class UserRegister(View):
 			messages.success(request, 'you registered successfully', 'info')
 			return redirect('core:home')
 		return render(request, self.template_name, {'form':form})
+
+
+class UserLogin(View):
+	form_class = UserLoginFrom
+	template_name = 'accounts/login.html'
+
+	def get(self, request):
+		form = self.form_class
+		return render(request, self.template_name, {'form':form})
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			user = authenticate(request, username=cd['username'], password=cd['password'])
+			if user is not None:
+				login(request, user)
+				messages.success(request, 'you logged in successfully', 'info')
+				return redirect('core:home')
+			messages.error(request, 'username or password is wrong', 'warning')
+		return render(request, self.template_name, {'form':form})
+
+
+class UserLogout(LoginRequiredMixin, View):
+	def get(self, request):
+		logout(request)
+		messages.success(request, 'you logged out successfully', 'info')
+		return redirect('core:home')
+
+
+class UserDashboard(LoginRequiredMixin, View):
+	template_name = 'accounts/dashboard.html'
+
+	def get(self, request, username):
+		user = get_object_or_404(User, username=username)
+		return render(request, self.template_name, {'user':user})
+
+	def post(self, request):
+		pass
+
+
+
+
+
+
+
+
