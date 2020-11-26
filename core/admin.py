075f823491e31e6a1d02b4848d32d133a8fbb80a
amirbigg <amirbig44@gmail.com>
2020-11-26 22:17:51 +0330

    add ep12

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Answer)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200)
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'{self.user} - {self.title[:20]}'
+
+
+class Answer(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+	question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'{self.user} - {self.question.title[:20]}'
