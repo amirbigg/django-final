d90d00ea421dc06fa5b1797199d7d6cec21dd43f
amirbigg <amirbig44@gmail.com>
2020-12-15 12:07:46 +0330

    add ep22

---
+++ accounts/tests/test_urls.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from accounts.views import UserRegister, UserDashboard
+
+
+class TestUrls(SimpleTestCase):
+	def test_register(self):
+		url = reverse('accounts:register') # /accounts/register
+		self.assertEqual(resolve(url).func.view_class, UserRegister)
+
+	def test_dashboard(self):
+		url = reverse('accounts:dashboard')
+		self.assertEqual(resolve(url).func.view_class, UserDashboard)
