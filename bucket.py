99ab7b5f5aa086922b209a3bea5a810fcd6da8a2
MrRezoo <rezam578@gmail.com>
2021-06-08 17:45:59 +0430

    The Bucket class converted to Singleton

--- bucket.py
+++ bucket.py
@@ -3,38 +3,46 @@ from django.conf import settings
 
 
 class Bucket:
-	"""CDN bucket manager
+    """CDN bucket manager
 
-	The init method creates connection
+    The init method creates connection
 
-	NOTE:
-		None of these methods are async. these methods are private interface.
-		use public interface in tasks module.
-	"""
+    This class use singleton pattern
 
-	def __init__(self):
-		session = boto3.session.Session()
-		self.conn = session.client(
-							service_name=settings.AWS_SERVICE_NAME,
-							aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-							aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-							endpoint_url=settings.AWS_S3_ENDPOINT_URL,
-							)
+    NOTE:
+        None of these methods are async. these methods are private interface.
+        use public interface in tasks module.
+    """
 
-	def get_objects(self):
-		result = self.conn.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
-		if result['KeyCount']:
-			return result['Contents']
-		else:
-			return None
+    @classmethod
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(*args, **kwargs)
+        return cls.instance
 
-	def delete_object(self, key):
-		self.conn.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
-		return True
+    def __init__(self):
+        session = boto3.session.Session()
+        self.conn = session.client(
+            service_name=settings.AWS_SERVICE_NAME,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        )
 
-	def download_object(self, key):
-		with open(settings.AWS_LOCAL_STORAGE + key, 'wb') as f:
-			self.conn.donwload_fileobj(settings.AWS_STORAGE_BUCKET_NAME, key, f)
+    def get_objects(self):
+        result = self.conn.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+        if result['KeyCount']:
+            return result['Contents']
+        else:
+            return None
+
+    def delete_object(self, key):
+        self.conn.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
+        return True
+
+    def download_object(self, key):
+        with open(settings.AWS_LOCAL_STORAGE + key, 'wb') as f:
+            self.conn.donwload_fileobj(settings.AWS_STORAGE_BUCKET_NAME, key, f)
 
 
 bucket = Bucket()
