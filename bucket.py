b69df426ce0b5d77dd75792b347cce28aa576867
Sami <73436220+mohsensami@users.noreply.github.com>
2022-03-09 19:48:19 +0330

    download_file
    https://stackoverflow.com/questions/29378763/how-to-save-s3-object-to-a-file-using-boto3

--- bucket.py
+++ bucket.py
@@ -34,7 +34,7 @@ class Bucket:
 
 	def download_object(self, key):
 		with open(settings.AWS_LOCAL_STORAGE + key, 'wb') as f:
-			self.conn.donwload_fileobj(settings.AWS_STORAGE_BUCKET_NAME, key, f)
+			self.conn.download_file(settings.AWS_STORAGE_BUCKET_NAME, key, f)
 
 
 bucket = Bucket()
